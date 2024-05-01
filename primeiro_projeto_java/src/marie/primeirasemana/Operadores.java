f2cbfa7855709eee5c389927da325516be5d2106
mariellemiziara <mariellemiziara@hotmail.com>
2024-05-01 17:35:33 -0300

    Create Operadores.java

---
+++ primeiro_projeto_java/src/marie/primeirasemana/Operadores.java
@@ -0,0 +1,19 @@
+package marie.primeirasemana;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=true;
+
+        if(condicao1 && ( 7 > 4 )){
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+
+    }
+}
