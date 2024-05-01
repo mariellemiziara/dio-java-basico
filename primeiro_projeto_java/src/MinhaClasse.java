30d2998fb3075666813ab48c541576a98127aa78
mariellemiziara <mariellemiziara@hotmail.com>
2024-05-01 11:41:38 -0300

    Update MinhaClasse.java

--- primeiro_projeto_java/src/MinhaClasse.java
+++ primeiro_projeto_java/src/MinhaClasse.java
@@ -1,8 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-       
-        System.out.print ( "Ola turma" );
-        
+       String primeiroNome = "Gleyson";
+       String segundoNome = "Sampaio";
+
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+       System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 } 
