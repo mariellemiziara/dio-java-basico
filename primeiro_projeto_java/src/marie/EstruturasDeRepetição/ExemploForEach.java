6b6dc5e016c8194afe023ac43f3a0d281fc6be32
mariellemiziara <mariellemiziara@hotmail.com>
2024-05-08 14:24:46 -0300

    .

---
+++ primeiro_projeto_java/src/marie/EstruturasDeRepetição/ExemploForEach.java
@@ -0,0 +1,11 @@
+public class ExemploForEach {// ExemploFor.java
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+        
+            //Forma abreviada
+        for(String aluno : alunos) {
+          System.out.println(alunos);
+        }
+    
+    }
+}
